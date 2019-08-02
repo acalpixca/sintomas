a3793bb16a00276de607e9ab1bcc2c736452137b
unknown <acalpixca@gmail.com>
2019-08-02 14:21:45 +0200

    include methods into constructor in sintoma.js

--- sintoma.js
+++ sintoma.js
@@ -4,22 +4,24 @@ function Sintoma(descripcion) {
   // always initialize all instance properties
 	this.descripcion = descripcion;
 	this.baGang = new BaGang();
-}
+
 // class methods
-Sintoma.prototype.setDescripcion = function(descripcion) {
-	this.descripcion=descripcion;
-};
+	this.setDescripcion = (descripcion) => {
+		this.descripcion=descripcion;
+	};
 
-Sintoma.prototype.getDescripcion= function(){
-	return(this.descripcion);
-}
+	this.getDescripcion= () => {
+		return(this.descripcion);
+	}
 
-Sintoma.prototype.setBaGang = function(baGang) {
-	this.baGang=baGang;
-}
+	this.setBaGang = (baGang) => {
+		this.baGang=baGang;
+	}
 
-Sintoma.prototype.getBaGang = function(){
-	return(this.baGang);
+	this.getBaGang = () => {
+		return(this.baGang);
+	}
+
 }
 // export the class
 
